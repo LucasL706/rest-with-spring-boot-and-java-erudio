2dce206dd5c71e758675219ca20cc660325c3aaa
LucasL <lucaslombardi706@gmail.com>
2026-03-04 15:14:08 -0300

    Adição de Migrations. Sessao-11

---
+++ SegundoPasso/src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+                                                                                (1, 'São Paulo - Brazil', 'Ayrton', 'Male', 'Senna'),
+                                                                                (3, 'Anchiano - Italy', 'Leonardo', 'Male', 'da Vinci'),
+                                                                                (4, 'Kentucky - US', 'Muhamamd', 'Male', 'Ali'),
+                                                                                (5, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela'),
+                                                                                (6, 'Mvezo - South Africa', 'Nelson', 'Male', 'Mandela');
